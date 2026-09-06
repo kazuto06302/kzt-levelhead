f776ccbf027c7fb9f27edf5852549ee476c0d4e0
kazuto06302 <galaxy0630wk@gmail.com>
2026-09-06 14:35:05 +0900

    2

---
+++ src/main/java/net/kztmc/mc/levelhead/api/HypixelApiException.java
@@ -0,0 +1,22 @@
+package net.kztmc.mc.levelhead.api;
+
+public class HypixelApiException extends Exception {
+
+    private final int statusCode;
+
+    public HypixelApiException(
+            int statusCode,
+            String message
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public boolean isRateLimited() {
+        return statusCode == 429;
+    }
+}
