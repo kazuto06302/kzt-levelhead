95bf0d74c323cd2974366862cdc9d57875b4e650
kazuto06302 <galaxy0630wk@outlook.jp>
2026-09-09 01:01:25 +0900

    14.2

--- src/main/java/net/kztmc/mc/levelhead/api/HypixelApiClient.java
+++ src/main/java/net/kztmc/mc/levelhead/api/HypixelApiClient.java
@@ -49,32 +49,17 @@ public class HypixelApiClient implements ApiClient {
             if (statusCode == 429) {
                 String body = readErrorStream(connection);
 
-                final String info =
+                LevelHeadCommand.send(Minecraft.getMinecraft().thePlayer,
                         "§6[I] HTTP " + statusCode +
                                 " | Remaining: " +
                                 connection.getHeaderField("RateLimit-Remaining") +
                                 " | Reset: " +
-                                connection.getHeaderField("RateLimit-Reset");
+                                connection.getHeaderField("RateLimit-Reset")
+                );
 
-                final String requestUuid =
-                        "§6[L] LevelHead API UUID: " + uuid;
-
-                Minecraft.getMinecraft().addScheduledTask(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (Minecraft.getMinecraft().thePlayer != null) {
-                            LevelHeadCommand.send(
-                                    Minecraft.getMinecraft().thePlayer,
-                                    info
-                            );
-
-                            LevelHeadCommand.send(
-                                    Minecraft.getMinecraft().thePlayer,
-                                    requestUuid
-                            );
-                        }
-                    }
-                });
+                LevelHeadCommand.send(Minecraft.getMinecraft().thePlayer,
+                        "§6[L] LevelHead API UUID: " + uuid
+                );
 
                 throw new HypixelApiException(
                         429,
